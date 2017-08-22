9c6c4c5e0ff00f7c54098ffe00ad37a92ffe1a0b
Daniel Onggunhao <danielo@cs.stanford.edu>
2017-08-23 04:12:53 +0700

    Albums now render using React Routes

--- browser/react/components/Main.js
+++ browser/react/components/Main.js
@@ -4,6 +4,13 @@ import AllAlbums from './AllAlbums';
 import SingleAlbum from './SingleAlbum';
 import Sidebar from './Sidebar';
 import Player from './Player';
+import {
+  // BrowserRouter as Router,
+  HashRouter,
+  Route
+  // Link
+} from 'react-router-dom'
+
 
 export default class Main extends Component {
 
@@ -43,13 +50,12 @@ export default class Main extends Component {
         <div className="col-xs-2">
           <Sidebar deselectAlbum={this.deselectAlbum} />
         </div>
-        <div className="col-xs-10">
-        {
-          this.state.selectedAlbum.id ?
-          <SingleAlbum album={this.state.selectedAlbum} /> :
-          <AllAlbums albums={this.state.albums} selectAlbum={this.selectAlbum} />
-        }
-        </div>
+        <HashRouter>
+          <div className="col-xs-10">
+            <Route exact path='/' component={AllAlbums} />
+            <Route path='/albums' component={AllAlbums} />
+          </div>
+        </HashRouter>
         <Player />
       </div>
     );
